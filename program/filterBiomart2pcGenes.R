7687ca45f020393b9c1503fc32c187b4111ec46b
Lea@Mac <leaseep@gmail.com>
2022-02-21 15:51:00 +0100

    helping scripts to produce raw data

---
+++ program/filterBiomart2pcGenes.R
@@ -0,0 +1,26 @@
+# Mouse Build: GRCm38
+# Go here to Ensembl and search for respecitve gtf file 
+# should be eventually the one used for calling the genes
+# hence this is guessing
+biomart=read.table("~/Downloads/Mus_musculus.GRCm38.102.gtf",skip = 13,sep="\t",header = F)
+extract_attributes <- function(gtf_attributes, att_of_interest){
+  att <- strsplit(gtf_attributes, "; ")
+  att <- gsub("\"","",unlist(att))
+  if(!is.null(unlist(strsplit(att[grep(att_of_interest, att)], " ")))){
+    return( unlist(strsplit(att[grep(att_of_interest, att)], " "))[2])
+  }else{
+    return(NA)}
+}
+colnames(biomart)=c("chr","source","type","start","end","score","strand","phase","attributes")
+table(biomart$type)
+# filter to gene
+biomart_gene=biomart[biomart$type=="gene",]
+biomart_gene$geneID<- unlist(lapply(biomart_gene$attributes, extract_attributes, "gene_id"))
+biomart_gene$gene_type<- unlist(lapply(biomart_gene$attributes, extract_attributes, "gene_biotype"))
+biomart_gene$gene_type=gsub(";","",biomart_gene$gene_type)
+table(biomart_gene$gene_type)
+biomart_gene=biomart_gene[biomart_gene$gene_type=="protein_coding",]
+
+##########
+# save Bimart protein coding genes as file for faste access
+write.csv(biomart_gene,file = "../data/Biomart_GRCm38_proteinCodingGenesOnly.csv")
