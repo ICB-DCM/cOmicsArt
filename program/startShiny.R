2870a044dd0545b81fa6dc408b38489e2175551d
Lea@Mac <leaseep@gmail.com>
2022-03-07 17:19:48 +0100

    adjusted to work independent

--- program/startShiny.R
+++ program/startShiny.R
@@ -1,3 +1,5 @@
+setwd("program")
+renv::restore(lockfile = "renv.lock")
 library(plotly)
 library(shiny)
 library(shinyWidgets)
@@ -7,15 +9,15 @@ library(DESeq2)
 ########
 # Set Up security 
 ########
-credentials <- data.frame(
-  user = c("Clivia", "Lea"), # mandatory
-  password = c("Cii@31", "Lea"), # mandatory
-  #start = c("2019-04-15"), # optinal (all others)
-  #expire = c(NA, "2019-12-31"),
-  admin = c(FALSE, TRUE),
-  comment = "Log In to Run secret Shiny",
-  stringsAsFactors = FALSE
-)
+# credentials <- data.frame(
+#   user = c("Clivia", "Lea"), # mandatory
+#   password = c("Cii@31", "Lea"), # mandatory
+#   #start = c("2019-04-15"), # optinal (all others)
+#   #expire = c(NA, "2019-12-31"),
+#   admin = c(FALSE, TRUE),
+#   comment = "Log In to Run secret Shiny",
+#   stringsAsFactors = FALSE
+# )
 source("fun_filterRNA.R")
 source("fun_plotPCA.R")
 source("fun_volcano.R")
