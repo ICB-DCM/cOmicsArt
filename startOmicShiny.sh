c65051ebf8ffeed7708b2e05a5c93f10528b0bf9
ShinyServer Lea <lea.seep@uni-bonn.de>
2022-06-10 10:47:56 +0200

    Update to newest server version

--- startOmicShiny.sh
+++ startOmicShiny.sh
@@ -2,7 +2,7 @@ echo Starting Shiny
 cd program
 git pull
 Rscript -e "library(renv)"
-Rscript -e "renv::init(bioconductor = TRUE)"
+# Rscript -e "renv::init(bioconductor = TRUE)"
 Rscript -e "renv::restore(lockfile='renv.lock')"
 Rscript -e "library(shiny)"
 screen -S OmicShiny -d -m Rscript -e "shiny::runApp('shinyApp',port=3939)"
