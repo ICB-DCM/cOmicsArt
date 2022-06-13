7cfbb3dcf2b10e5cadd1697f3c540dec7492f37a
Lea@Mac <leaseep@gmail.com>
2022-06-13 13:50:57 +0200

    added the biconductor True line

--- startOmicShiny.sh
+++ startOmicShiny.sh
@@ -5,6 +5,7 @@ Rscript -e "library(renv)"
 # Rscript -e "renv::init(bioconductor = TRUE)"
 Rscript -e "print(getwd())"
 Rscript -e "renv::restore(lockfile='renv.lock')"
+RScript -e "renv::init(bioconductor = TRUE)"
 Rscript -e "library(shiny)"
 screen -S OmicShiny -d -m Rscript -e "shiny::runApp('shinyApp',port=3939)"
 echo running...
