047aee8a666c098052783950def7c0fda5ad1813
Lea@Mac <leaseep@gmail.com>
2022-06-13 13:45:19 +0200

    added print of current dir

--- startOmicShiny.sh
+++ startOmicShiny.sh
@@ -3,6 +3,7 @@ cd program
 git pull
 Rscript -e "library(renv)"
 # Rscript -e "renv::init(bioconductor = TRUE)"
+Rscript -e "print(getwd())"
 Rscript -e "renv::restore(lockfile='renv.lock')"
 Rscript -e "library(shiny)"
 screen -S OmicShiny -d -m Rscript -e "shiny::runApp('shinyApp',port=3939)"
