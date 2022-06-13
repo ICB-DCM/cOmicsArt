8f37b654d604fcfd8bc6055a4438fb7023f1a715
Lea@Mac <leaseep@gmail.com>
2022-06-13 13:53:47 +0200

    changed order of inits and restore

--- startOmicShiny.sh
+++ startOmicShiny.sh
@@ -4,8 +4,8 @@ git pull
 Rscript -e "library(renv)"
 # Rscript -e "renv::init(bioconductor = TRUE)"
 echo pwd
-Rscript -e "renv::restore(lockfile='renv.lock')"
 RScript -e "renv::init(bioconductor = TRUE)"
+Rscript -e "renv::restore(lockfile='renv.lock')"
 Rscript -e "library(shiny)"
 screen -S OmicShiny -d -m Rscript -e "shiny::runApp('shinyApp',port=3939)"
 echo running...
