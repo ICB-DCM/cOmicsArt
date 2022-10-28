87bf99dc0c39c9c4b63d7f7856ee99bc18879f32
PaulJonasJost <jost.pauljonas@gmail.com>
2022-10-28 15:50:53 +0200

    UI splitting

---
+++ program/shinyApp/R/data_selection/ui.R
@@ -0,0 +1,92 @@
+data_selection_sidebar_panel <- sidebarPanel(
+  id = "sidebar_data_selection",
+  div(class = "omicType",
+      selectInput(
+        inputId = "omicType", # RNAorLIPID
+        label = "Omic Type that is uploaded",
+        choices = c("Transcriptomics", "Lipidomics", "Metabolomics"),
+        selected = ""
+  )),
+  div(class = "AddGeneSymbols_ui",uiOutput("AddGeneSymbols_ui"),uiOutput("AddGeneSymbols_organism_ui")),
+  #uiOutput("AddGeneSymbols_organism_ui"),
+  actionButton(inputId = "refresh1", label = "Do"),
+  div(class="LineToDistinguish",hr(style = "border-top: 1px solid #000000;")),
+  div(class="DataSelection",
+      h4("Row selection -  biochemical entities"),
+      uiOutput("providedRowAnnotationTypes_ui"),
+      uiOutput("row_selection_ui"),
+      uiOutput("propensityChoiceUser_ui")),
+      # Outlier Selection -> for fixed removal pre-processing needs to be redone!
+  div(class="SampleSelection",
+      h4("Sample selection"),
+      uiOutput("providedSampleAnnotationTypes_ui"),
+      uiOutput("sample_selection_ui")),
+    uiOutput("NextPanel_ui")
+)
+
+
+data_selection_main_panel <- mainPanel(
+  id="mainPanel_DataSelection",
+  tabsetPanel(
+    type = "pills",
+    tabPanel(
+  title="Upload section",
+  splitLayout(
+    style = "border: 1px solid silver:", cellWidths = c("85%", "10%", "5%"),
+    NULL,
+    actionButton(inputId = "Reset", label = "Reset"),
+    NULL
+  ),
+  splitLayout(
+    style = "border: 1px solid silver:", cellWidths = c("50%", "50%"),
+    uiOutput("data_matrix1_ui"),
+    uiOutput("data_sample_anno1_ui")
+  ),
+  splitLayout(
+    style = "border: 1px solid silver:", cellWidths = c("50%", "50%"),
+    uiOutput("data_row_anno1_ui"),
+    uiOutput("data_preDone_ui") %>% helper(type = "markdown", content = "SummarizedExp_help")
+  ),
+  hr(style = "border-top: 2px solid #cbedca;"),
+  uiOutput("metadataInput_ui"),
+  hr(style = "border-top: 2px solid #cbedca;"),
+  downloadButton("SaveInputAsList", label = "Save file input to upload later") %>% helper(type = "markdown", content = "compilation_help"),
+  htmlOutput("debug", container = pre),
+  HTML("<br>"),
+  HTML("<br>")),
+  tabPanel(
+    title="Upload visual inspection",
+    helpText("If you have uploaded your data, you might want to visually check the tables to confirm the correct data format. If you notice irregualarities you will need to correct the input data - this cannot be done in ShinyOmics, See the help on how your data is expected."),
+    actionButton(inputId = "DoVisualDataInspection", label = "Upload data for visual inspection"),
+    splitLayout(
+      style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+      DT::dataTableOutput("DataMatrix_VI"),
+      htmlOutput("DataMatrix_VI_Info", container = pre)
+    ),
+    splitLayout(
+      style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+      DT::dataTableOutput("SampleMatrix_VI"),
+      htmlOutput("SampleMatrix_VI_Info", container = pre)
+    ),
+    splitLayout(
+      style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+      DT::dataTableOutput("EntitieMatrix_VI"),
+      htmlOutput("EntitieMatrix_VI_Info", container = pre)
+    ),
+    htmlOutput("OverallChecks", container = pre)
+  )
+  )
+)
+
+
+data_selection_panel <- tabPanel(
+  "Data selection",
+  id= "Data_selection",
+  fluid = T,
+  h4("Data Selection"),
+  ################################################################################
+  # Data Selection
+  ################################################################################
+  data_selection_sidebar_panel,
+  data_selection_main_panel
+)
---
+++ program/shinyApp/R/volcano_plot/ui.R
@@ -0,0 +1,75 @@
+volcano_plot_sidebar <- sidebarPanel(
+  id = "sidebar_volcano_plot",
+  #########################################
+  # VOLCANO
+  #########################################
+  uiOutput("sample_annotation_types_cmp_ui"),
+  uiOutput("Groups2Compare_ref_ui"),
+  uiOutput("Groups2Compare_treat_ui"),
+  actionButton(
+    inputId = "Do_Volcano",
+    label = "Do Volcano Plot",
+    icon("fas fa-laptop-code")
+  ),
+  hr(style = "border-top: 1px solid #000000;"),
+  uiOutput("psig_threhsold_ui"),
+  uiOutput("lfc_threshold_ui"),
+  switchInput(
+    inputId = "get_entire_table",
+    label = "show next to LFCs also the raw data that was used to calculate",
+    inline = T
+  )
+)
+
+
+volcano_plot_main <- mainPanel(
+  id = "main_volcano_plot",
+  tabsetPanel(
+    tabPanel(
+      "Volcano_Plot", plotlyOutput("Volcano_Plot_final") %>% withSpinner(type = 8),
+      uiOutput("VOLCANO_anno_tooltip_ui"),
+      splitLayout(
+        style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+        NULL,
+        actionButton(inputId = "only2Report_Volcano", label = "Send only to Report", class = "btn-info"),
+      ),
+      splitLayout(
+        style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+        NULL,
+        downloadButton("getR_Code_Volcano", label = "Get underlying R code and data",icon = icon("code"))
+      ),
+      splitLayout(
+        style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+        NULL,
+        downloadButton("SavePlot_Volcano", label = "Save plot", class = "btn-info")
+      ),
+      splitLayout(
+        style = "border: 1px solid silver:", cellWidths = c("70%", "30%"),
+        NULL,
+        radioGroupButtons(
+          input = "file_ext_Volcano", label = "File Type:",
+          choices = c(".png", ".tiff", ".pdf"), selected = ".png"
+        )
+      ),
+      splitLayout(
+        style = "border: 1px solid silver:", cellWidths = c("50%", "50%"),
+        downloadButton("SaveDE_List", label = "Save intresting entities (all red points)"),
+        actionButton(inputId = "SendDE_Genes2Enrichment", label = "Send DE Genes to enrichment analysis", block = F)
+      )
+    ),
+    tabPanel("Volcano_table", DT::dataTableOutput("Volcano_table_final")),
+
+  ),
+  textAreaInput(inputId="NotesVolcano", label="Notes:", placeholder="Notes you want to take alongside the Plot (will be saved in the report) \nYou may want to use markdown syntay for structering the notes ", width = "1000px")%>% helper(type = "markdown", content = "TakingNotesMD_help"),
+  helpText("Notes: For structure reasons you should start with Heading Level 4 (hence #### My personal Title)")
+)
+
+
+volcano_plot_panel <- tabPanel(
+  "Volcano Plot",
+  id = "volcano",
+  fluid = T,
+  h4("Volcano Plot"),
+  volcano_plot_sidebar,
+  volcano_plot_main
+)
