1a82da90b484c2379c2cacec07329b1c307291b9
PaulJonasJost <jost.pauljonas@gmail.com>
2022-11-04 15:53:10 +0100

    Umstyling module_DownloadReport.R

--- program/shinyApp/R/module_DownloadReport.R
+++ program/shinyApp/R/module_DownloadReport.R
@@ -1,9 +1,12 @@
 # Report Download module ----
 # ui ----
-DownloadReport_ui<-function(id){
+DownloadReport_ui <- function(id){
   ns <- NS(id)
   tagList(
-    actionLink(inputId = ns("DownloadReport"), label = "Download Report (as html)"),
+    actionLink(
+      inputId = ns("DownloadReport"),
+      label = "Download Report (as html)"
+    )
   )
 }
 
@@ -14,20 +17,29 @@ DownloadReport_server<-function(id){
     function(input,output,session){
       observeEvent(input$DownloadReport,{
         if(file.exists("./www/Report.md")){
-          show_toast("Generating Report....please wait",
-                     type = "info",
-                     position = "top",
-                     timerProgressBar = TRUE,
-                     width = "30%")
-          rmarkdown::render("./www/Report.md",html_document(toc = TRUE, toc_float = T ,fig_caption = T))
+          show_toast(
+            title = "Generating Report....please wait",
+            type = "info",
+            position = "top",
+            timerProgressBar = TRUE,
+            width = "30%"
+          )
+          rmarkdown::render(
+            input = "./www/Report.md",
+            html_document(toc = TRUE, toc_float = T ,fig_caption = T)
+          )
           showModal(modalDialog(
             tags$h4(a(href="Report.html", "Download report", download=NA, target="_blank")),
             footer=tagList(
-              modalButton('Return')
+              modalButton(label = 'Return')
             )))
         }else{
           warning("No Report File yet! Do something first")
-          shinyalert("Warning", "No Report File yet! Do something first", type = "warning")
+          shinyalert(
+            title = "Warning",
+            text = "No Report File yet! Do something first",
+            type = "warning"
+          )
         }
       })
     }
