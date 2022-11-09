79f89fe12be2401066fe876ab857e36d2598f899
Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
2022-11-09 09:26:31 +0100

    Select gsea methods (#41)
    * Completely modularize ea
    
    * Buttons to choose which enrichments to display. still needs to be fixed: do only them.
    
    * Only do the requested analysis
    
    * Update program/shinyApp/R/enrichment_analysis/ui.R
    
    Co-authored-by: Lea Seep <74967328+LeaSeep@users.noreply.github.com>
    
    Co-authored-by: Lea Seep <74967328+LeaSeep@users.noreply.github.com>

--- program/shinyApp/R/enrichment_analysis/enrichment_analysis.R
+++ program/shinyApp/R/enrichment_analysis/enrichment_analysis.R
@@ -25,150 +25,167 @@ gene_set_enrichment <- function(
   EnrichmentRes_C7 <- NULL
   EnrichmentRes_C8 <- NULL
 
-
-  EnrichmentRes_Kegg <- clusterProfiler::gseKEGG(
-    geneList = geneSetChoice_tranlsated,
-    keyType = "ncbi-geneid",  # equal to ENTREZID
-    organism = ifelse(input$OrganismChoice == "hsa","hsa","mmu"),
-    minGSSize = 3,
-    maxGSSize = 800,
-    pvalueCutoff = 0.05,
-    verbose = TRUE,
-    pAdjustMethod = "BH"
-  )
-  # EnrichmentRes_GO <- clusterProfiler::gseGO(
-  #   gene = geneSetChoice_tranlsated,
-  #   ont = input$ontologyForGO,
-  #   keyType = "ENTREZID",
-  #   minGSSize = 3,
-  #   maxGSSize = 800,
-  #   pvalueCutoff = 0.05,
-  #   verbose = TRUE,
-  #   OrgDb = ifelse(input$OrganismChoice=="hsa","org.Hs.eg.db","org.Mm.eg.db"),
-  #   pAdjustMethod = "none"  # TODO: discuss
-  # )
-  #
-  # # TODO: add correct buttons and Tabs with Visualisation for all Pathways
-  # # Hallmarks
-  # Hallmarkset <- msigdbr(
-  #   species = "Mus musculus",
-  #   category = "H",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_Hallmarks <- GSEA(
-  #   geneSetChoice_tranlsated,
-  #   TERM2GENE  =  Hallmarkset,
-  #   verbose = FALSE,
-  #   eps = 0,
-  #   pAdjustMethod = 'bonferroni',
-  #   pvalueCutoff = 1
-  # )
-  # browser()
-  # # C1
-  # C1set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C1",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C1 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C1set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  #   )
-  # # C2
-  # C2set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C2",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C2 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C2set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-  # # C3
-  # C3set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C3",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C3 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C3set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-  # # C4
-  # C4set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C4",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C4 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C4set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-  # # C5
-  # C5set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C5",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C5 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C5set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-  # # C6
-  # C6set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C6",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C6 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C6set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-  # C7 ImmuneSigDB subset
-  C7set <- msigdbr(
-    species = "Mus musculus",
-    category = "C7",
-    subcategory = "IMMUNESIGDB"
-  )%>% dplyr::select(gs_name, entrez_gene)
-  EnrichmentRes_C7 <- GSEA(
+  if(global_Vars$enrichments2do$KEGG){
+    EnrichmentRes_Kegg <- clusterProfiler::gseKEGG(
+      geneList = geneSetChoice_tranlsated,
+      keyType = "ncbi-geneid",  # equal to ENTREZID
+      organism = ifelse(input$OrganismChoice == "hsa","hsa","mmu"),
+      minGSSize = 3,
+      maxGSSize = 800,
+      pvalueCutoff = 0.05,
+      verbose = TRUE,
+      pAdjustMethod = "BH"
+    )
+  }
+  if(global_Vars$enrichments2do$GO){
+    EnrichmentRes_GO <- clusterProfiler::gseGO(
+      gene = geneSetChoice_tranlsated,
+      ont = input$ontologyForGO,
+      keyType = "ENTREZID",
+      minGSSize = 3,
+      maxGSSize = 800,
+      pvalueCutoff = 0.05,
+      verbose = TRUE,
+      OrgDb = ifelse(input$OrganismChoice=="hsa","org.Hs.eg.db","org.Mm.eg.db"),
+      pAdjustMethod = "none"  # TODO: discuss
+    )
+  }
+  if(global_Vars$enrichments2do$Hallmarks){
+    # Hallmarks
+    Hallmarkset <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "H",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_Hallmarks <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE  =  Hallmarkset,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C1){
+    # C1
+    C1set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C1",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C1 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C1set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C2){
+    # C2
+    C2set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C2",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C2 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C2set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C3){
+    # C3
+    C3set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C3",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C3 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C3set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C4){
+    # C4
+    C4set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C4",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C4 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C4set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C5){
+    # C5
+    C5set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C5",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C5 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C5set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C6){
+    # C6
+    C6set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C6",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C6 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C6set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
+  if(global_Vars$enrichments2do$C7){
+    # C7 ImmuneSigDB subset
+    C7set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C7",
+      subcategory = "IMMUNESIGDB"
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C7 <- GSEA(
       geneSetChoice_tranlsated,
       TERM2GENE = C7set,
       verbose = FALSE,
       eps = 0,
       pAdjustMethod = 'bonferroni',
       pvalueCutoff = 1
-  )
-  # # C8
-  # C8set <- msigdbr(
-  #     species = "Mus musculus",
-  #     category = "C8",
-  # )%>% dplyr::select(gs_name, entrez_gene)
-  # EnrichmentRes_C8 <- GSEA(
-  #     geneSetChoice_tranlsated,
-  #     TERM2GENE = C8set,
-  #     verbose = FALSE,
-  #     eps = 0,
-  #     pAdjustMethod = 'bonferroni',
-  #     pvalueCutoff = 1
-  # )
-
+    )
+  }
+  if(global_Vars$enrichments2do$C8){
+    # C8
+    C8set <- msigdbr(
+      species = ifelse(input$OrganismChoice == "hsa","Homo sapiens","Mus musculus"),
+      category = "C8",
+    )%>% dplyr::select(gs_name, entrez_gene)
+    EnrichmentRes_C8 <- GSEA(
+      geneSetChoice_tranlsated,
+      TERM2GENE = C8set,
+      verbose = FALSE,
+      eps = 0,
+      pAdjustMethod = 'bonferroni',
+      pvalueCutoff = 1
+    )
+  }
 
   return(list(
     "EnrichmentRes_KEGG" = EnrichmentRes_Kegg,
