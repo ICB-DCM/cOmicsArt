aea6e3f6c43091b4ae1cc5c427a706ef3aed6354
Lea@Mac <leaseep@gmail.com>
2022-12-02 11:14:59 +0100

    specify namespace

--- program/shinyApp/R/significance_analysis/server.R
+++ program/shinyApp/R/significance_analysis/server.R
@@ -318,7 +318,7 @@ significance_analysis_server <- function(id, preprocess_method, omic_type){
         # plot the results
         if(input$visualization_method == "UpSetR plot"){
           sig_ana_reactive$plot_last <- UpSetR::upset(
-              fromList(res2plot)
+            UpSetR::fromList(res2plot)
             )
           output$Significant_Plot_final <- renderPlot({
             sig_ana_reactive$plot_last
