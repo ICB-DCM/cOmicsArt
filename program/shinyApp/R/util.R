96cdcc71c513f14fced1eb4869c72479c5755ce5
Lea@Mac <leaseep@gmail.com>
2023-03-03 15:56:41 +0100

    fix not updating issue

--- program/shinyApp/R/util.R
+++ program/shinyApp/R/util.R
@@ -3,7 +3,10 @@
 
 update_data <- function(data, updates, current_updates){
   # update data if updates is larger than current_updates
-  if (updates() > current_updates & current_updates > 0){
+  # could force to always update
+  if (
+    #updates() > current_updates & 
+    current_updates > 0){
     print("Updating data...")
     data <- res_tmp
   }
