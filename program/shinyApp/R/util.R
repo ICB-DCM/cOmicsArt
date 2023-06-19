37191f503e584431bc37e58e12ec04f3e3ec31d5
Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
2023-06-19 15:41:09 +0200

    enables excel upload (#138)

--- program/shinyApp/R/util.R
+++ program/shinyApp/R/util.R
@@ -39,3 +39,30 @@ update_params <- function(params, updates, current_updates){
   }
   return(params)
 }
+
+
+read_file <- function(filename, check.names=T){
+  # reads in the file of either a .csv or a .xlsx filetype
+  if (base::endsWith(filename, ".csv")){
+    df <- read.csv(
+       file = filename,
+       header = T,
+       row.names = 1,
+       check.names = check.names
+    )
+    return(df)
+  }
+  if (base::endsWith(filename, ".xlsx")){
+    df <- as.data.frame(
+      readxl::read_xlsx(
+        path=filename,
+        col_names=TRUE,
+        na="NA",
+        .name_repair = ifelse(check.names, "unique", "minimal")
+        )
+      )
+    rownames(df) <- df[[1]]
+    df[[1]] <- NULL
+    return(df)
+  }
+}
