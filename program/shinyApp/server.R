47cf787bea8eb7735927f0977dadfe9370172748
Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
2024-01-23 15:00:52 +0100

    Introduce updating scheme for ea. (#153)
    * Introduce updating scheme for ea.
    
    * Update program/shinyApp/R/enrichment_analysis/server.R
    
    Co-authored-by: Lea Seep <74967328+LeaSeep@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lea Seep <74967328+LeaSeep@users.noreply.github.com>

--- program/shinyApp/server.R
+++ program/shinyApp/server.R
@@ -974,6 +974,7 @@ server <- function(input,output,session){
       
       shinyjs::click("SignificanceAnalysis-refreshUI",asis = T)
       shinyjs::click("single_gene_visualisation-refreshUI",asis = T)
+      shinyjs::click("EnrichmentAnalysis-refreshUI",asis = T)
       paste0(addWarning,
              "The data has the dimensions of: ",
              paste0(dim(res_tmp$data),collapse = ", "),
