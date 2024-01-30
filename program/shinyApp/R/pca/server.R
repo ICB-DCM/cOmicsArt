70da7b40ee052379c0e61022da43413a494c8d02
PaulJonasJost <jost.pauljonas@gmail.com>
2024-01-30 17:25:11 +0100

    Reverted wrong change.

--- program/shinyApp/R/pca/server.R
+++ program/shinyApp/R/pca/server.R
@@ -169,7 +169,7 @@ pca_Server <- function(id, data, params, row_select){
           "PCA - ", params$omic_type, "-",
           paste0("entities:",row_select(),collapse = "_"),
           "-samples",
-          ifelse(any(input$sample_selection != "all"),paste0(" (with: ",paste0(input$sample_selection,collapse = ", "),")"))
+          ifelse(any(input$sample_selection != "all"),paste0(" (with: ",paste0(input$sample_selection,collapse = ", "),")"),"")
           , "-preprocessing: ",
           input$PreProcessing_Procedure
         )
