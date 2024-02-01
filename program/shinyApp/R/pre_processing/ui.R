6d95c0b8297d35ec05d6e79167f18d98c8d05b7e
PaulJonasJost <jost.pauljonas@gmail.com>
2024-02-01 14:59:04 +0100

    UI cleanup.

--- program/shinyApp/R/pre_processing/ui.R
+++ program/shinyApp/R/pre_processing/ui.R
@@ -36,16 +36,9 @@ pre_processing_main_panel <- mainPanel(
   id = "mainpanel_pre_processing",
   # Statistics to the data
   helpText("general statistics to the input data, stuff like dimensions"),
-  # hidden(div(id = 'Spinner_Statisitcs_Data', plotOutput("Statisitcs_Data")%>% withSpinner(type=8))),
   htmlOutput(outputId = "Statisitcs_Data") %>% withSpinner(type = 8),
   HTML(text = "<br>"),
-  HTML(text = "<br>"),
-  splitLayout(
-    cellWidths = c("25%", "25%", "25%"),
-    # uiOutput(outputId = "NextPanel2_ui"),
-    # uiOutput(outputId = "NextPanel3_ui"),
-    # uiOutput(outputId = "NextPanel4_ui")
-  )
+  HTML(text = "<br>")
 )
 
 
@@ -53,7 +46,7 @@ pre_processing_panel <- tabPanel(
   title = "Pre-processing",
   id = "pre_processing_panel",
   fluid = T,
-  h4("Data Pre-processing"),  # %>% helper(type = "markdown", content = "PreProcessing_help"),
+  h4("Data Pre-processing"),
   pre_processing_sidebar_panel,
   pre_processing_main_panel
 )
