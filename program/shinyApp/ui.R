7dfec168433fc252301e597a3fe280a6256d27d1
PaulJonasJost <jost.pauljonas@gmail.com>
2024-02-01 14:59:44 +0100

    Removed unused security

--- program/shinyApp/ui.R
+++ program/shinyApp/ui.R
@@ -59,19 +59,6 @@ source("R/significance_analysis/ui.R",local=T)
 
 options(repos = BiocManager::repositories())
 options(spinner.color = "#1c8a3b", spinner.color.background = "#ffffff", spinner.size = 2)
-########
-# Set Up security
-########
-credentials <- data.frame(
-  user = c("Clivia", "Lea"), # mandatory
-  password = c("Cii@31", "Lea"), # mandatory
-  # start = c("2019-04-15"), # optinal (all others)
-  # expire = c(NA, "2019-12-31"),
-  admin = c(FALSE, TRUE),
-  comment = "Log In to Run secret Shiny",
-  stringsAsFactors = FALSE
-)
-
 
 ui <- shiny::fluidPage(
   # JS to reset input values
