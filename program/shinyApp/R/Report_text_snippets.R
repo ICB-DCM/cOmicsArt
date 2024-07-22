20db50f8d208290fbf5832192910b9c97121970e
Lea@Mac <leaseep@gmail.com>
2024-07-22 11:35:16 +0200

    data selection tab report clean up

--- program/shinyApp/R/Report_text_snippets.R
+++ program/shinyApp/R/Report_text_snippets.R
@@ -18,15 +18,34 @@ clean_citation <- function(citation) {
 ## done as functions
 ## Input. packages 
 snippet_dataInput <- function(
-  data_type,
-  data_dimension,
-  package_list = c(purrr = "purrr")
+  data=res_tmp[[session$token]],
+  params=par_tmp[[session$token]]
 ){
+  browser()
   snippet <- 
-    paste0("The ", data_type ," data was read into R (v. ",getRversion(),") (",clean_citation(citation()),").", 
-           " The raw's data dimensions were: ", data_dimension,
-           ". All annotation that is constant over all samples is hidden within the Shiny-Application, as they do not provide any additional knowledge.",
-           "This was done with the purrr package (v. ",packageVersion(package_list['purrr']),")(",
-           print(clean_citation(citation(package_list['purrr'])), style = "text"),")")
+    paste0("The data was uploaded to cOmicsART (v. ", VERSION,") a webapp to perform explorative and statistical analysis with seamless integration to R (Seep et. al. 2024). ",
+           "The webapp is majorly built with the shiny package (v. ",packageVersion("shiny"),") (",print(clean_citation(citation('shiny')), style = "text"),"). ",
+           "It is currently running on R (v. ", R.version$major, ".", R.version$minor, ") (", print(clean_citation(citation('base')), style = "text"),"). ",
+           "The ", params$omic_type ," data was uploaded with the original dimensions of ", dim(data$data_original)[1], " features and ", dim(data$data_original)[2], " samples. ",
+           if(params$addedGeneAnno){
+             paste0("Gene annotation was added using the ",params$organism ,"mart from Ensembl implemented within the biomaRt package 
+                    (v. ",packageVersion("biomaRt"),") (",print(clean_citation(citation('biomaRt')), style = "text"),"). ")
+             },
+           if(any(params$sample_selection == "all")){
+             paste0("No sample selection was performed. ")
+           }else{
+             paste0("All samples with",params$providedSampleAnnotationTypes,"being ",paste(params$sample_selection,collapse = ", "),"were selected. ")
+           },
+           if(any(params$row_selection == "all")){
+             paste0("No entitie selection was performed. ")
+           }else{
+             if(any(params$row_selection == "High Values+IQR")){
+               paste0("Selects the top entities based on their maximum expression levels and interquartile range (IQR), returning the intersection of genes that exceed the ",params$propensity, " quantile for both criteria. ")
+             }else{
+               paste0("All entities with ",params$providedRowAnnotationTypes," being ",paste(params$row_selection,collapse = ", ")," were selected. ")
+             }
+           }
+           )
   return(snippet)
 }
+
