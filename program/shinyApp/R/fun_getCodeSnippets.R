954143aae1cde5322752dd7e26542320bd1c3538
Lea@Mac <leaseep@gmail.com>
2024-08-08 19:42:26 +0200

    some vizz

--- program/shinyApp/R/fun_getCodeSnippets.R
+++ program/shinyApp/R/fun_getCodeSnippets.R
@@ -150,36 +150,32 @@ selected <- unique(
     stringPreProcessing <- 'res_tmp$data <- tmp_data_selected'
   }
   if(par_tmp[[session$token]]['BatchColumn'] != "NULL" & PreProcessing_Procedure != "vst_DESeq"){
-    string_batchCorrection <- 'res_tmp$data_batch_corrected <- res_tmp$data
-      assay(res_tmp$data_batch_corrected) <- sva::ComBat(
-      dat = assay(res_tmp$data_batch_corrected),
-      batch = as.factor(colData(res_tmp$data_batch_corrected)[,par_tmp$BatchColumn])
-    )
-    '
+    string_batchCorrection <- '
+res_tmp$data_batch_corrected <- res_tmp$data
+assay(res_tmp$data_batch_corrected) <- sva::ComBat(
+  dat = assay(res_tmp$data_batch_corrected),
+  batch = as.factor(colData(res_tmp$data_batch_corrected)[,par_tmp$BatchColumn])
+)
+'
    # string_batchCorrection <- paste0(prequel_stringPreProcessing,"\n", string_batchCorrection)
   } else if (par_tmp[[session$token]]['BatchColumn'] != "NULL" & PreProcessing_Procedure == "vst_DESeq") {
     # formula needs to be updated
-    string_batchCorrection <- 'dds_batch <- DESeq2::DESeqDataSetFromMatrix(
-          countData = assay(res_tmp$data),
-          colData = colData(res_tmp$data),
-          design = as.formula(par_tmp$DESeq_formula_batch)
-        )
-      de_seq_result_batch <- DESeq2::DESeq(dds_batch)
-      res_tmp$DESeq_obj_batch <- de_seq_result_batch
-      dds_vst_batch <- vst(
-      object = de_seq_result,
-      blind = TRUE
-      )
-    # Note that the following command overwrites the current data with the batch corrected data
-    # if you want to recover the non batch corrected data you can check dds_vst
-      assay(res_tmp$data) <- as.data.frame(assay(de_seq_result_batch))
+    string_batchCorrection <- '
+dds_batch <- DESeq2::DESeqDataSetFromMatrix(
+      countData = assay(res_tmp$data),
+      colData = colData(res_tmp$data),
+      design = as.formula(par_tmp$DESeq_formula_batch)
+    )
+de_seq_result_batch <- DESeq2::DESeq(dds_batch)
+res_tmp$DESeq_obj_batch <- de_seq_result_batch
+dds_vst_batch <- vst(
+object = de_seq_result,
+blind = TRUE
+)
+# Note that the following command overwrites the current data with the batch corrected data
+# if you want to recover the non batch corrected data you can check dds_vst
+assay(res_tmp$data) <- as.data.frame(assay(de_seq_result_batch))
       '
-    #stringPreProcessing_batch <- stringPreProcessing
-    # pre-processing needs to be redone here bzw checked up front
-    
-    #stringPreProcessing_batch <- gsub("res_tmp$data","res_tmp$data_batch_corrected",stringPreProcessing_batch)
-    #stringPreProcessing_batch <- gsub("par_tmp$DESeq_formula","par_tmp$DESeq_formula_batch",stringPreProcessing_batch)
-   # string_batchCorrection <- paste0(prequel_stringPreProcessing,"\n", string_batchCorrection)
   } else {
       string_batchCorrection <- ''
   }
@@ -187,7 +183,7 @@ selected <- unique(
   if (par_tmp[[session$token]]['BatchColumn'] != "NULL") {
     stringPreProcessing <- paste0(
       stringPreProcessing, "\n",
-      "# uncomment this line to use batch corrected data\n# res_tmp$data <- res_tmp$data_batch_corrected\n"
+      "# uncomment this line to use non - batch corrected data\nres_tmp$data <- res_tmp$data_batch_corrected\n"
     )
   }
     
