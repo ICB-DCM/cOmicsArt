cf5c5be55f4a65a3176d753b2c906d0f1eac4db7
Lea Seep <74967328+LeaSeep@users.noreply.github.com>
2024-09-09 16:39:10 +0200

    all leaseep to icb-dcm (#340)
    consequence of move from personal repo to organisation

--- program/shinyApp/R/version.R
+++ program/shinyApp/R/version.R
@@ -1,3 +1,3 @@
 VERSION <<- "v0.1.0"
-DOCUMENTATION <<- "https://leaseep.github.io/cOmicsArt/welcome.html"
+DOCUMENTATION <<- "https://icb-dcm.github.io/cOmicsArt/welcome.html"
 SERVER <<- "https://shiny.iaas.uni-bonn.de/cOmicsArt/"
