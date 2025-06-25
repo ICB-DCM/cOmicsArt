603258251b187f207639ce782cb5ee98421ee90c
anikde07 <s94ade@uni-bonn.de>
2025-06-25 12:29:29 +0200

    Added copying for single gene visualisation

--- program/shinyApp/R/pca/server.R
+++ program/shinyApp/R/pca/server.R
@@ -214,8 +214,32 @@ pca_Server <- function(id){
     })
     output$PCA_Loadings_matrix_plot <- renderPlotly({
       req(pca_reactives$allow_plot)
-      pca_reactives$LoadingsMatrix_plot
-      ggplotly(pca_reactives$LoadingsMatrix_plot)
+      p  = ggplotly(pca_reactives$LoadingsMatrix_plot)
+      onRender(p, "
+        function(el, x) {
+          Plotly.newPlot(el, x.data, x.layout, {
+            modeBarButtonsToAdd: [{
+              name: 'Copy to clipboard',
+              icon: Plotly.Icons.camera,  // placeholder icon
+              click: function(gd) {
+                Plotly.toImage(gd, {format: 'png'}).then(function(url) {
+                  fetch(url)
+                    .then(res => res.blob())
+                    .then(blob => {
+                      navigator.clipboard.write([
+                        new ClipboardItem({ [blob.type]: blob })
+                      ]).then(function() {
+                        Shiny.setInputValue('plot_copied_status', 'PCA_loadings_matrix_plot_success_' + Date.now(), {priority: 'event'});
+                      }).catch(function(err) {
+                        Shiny.setInputValue('plot_copied_status', 'PCA_loadings_matrix_plot_clipboard_error_' + Date.now(), {priority: 'event'});
+                      });
+                    });
+                });
+              }
+            }]
+          });
+        }
+      ")
     })
 
     observeEvent(input$Do_PCA,{  # Calculate values needed for PCA
