3d3e845d615eb4a8522b4954ecea68212e84fe57
Lea@Mac <leaseep@gmail.com>
2026-07-02 12:53:23 +0200

    actually performs selection

--- program/shinyApp/R/util.R
+++ program/shinyApp/R/util.R
@@ -59,7 +59,7 @@ select_data <- function(
       ))
   }
   rows_selected <- c()
-  if(any(selected_rows == "all")){
+  if(any(selected_rows == "all") | length(selected_rows) == nrow(assay(data))) {
     rows_selected <- rownames(data)
   } else if ("High Values+IQR" %in% selected_rows && length(selected_rows) == 1) {
     # Do nothing, as we don't want to modify `selected` in this case
